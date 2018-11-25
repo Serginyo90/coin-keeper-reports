7d99ff026486e4d9be23e7ff32028a9a8120113d
Sergey Cherbadzhy <s.cherbadzhy@gmail.com>
2018-11-25 19:33:05 +0200

    initialization

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ import './App.css';
 
 class App extends Component {
   state = {
-    text: null,
+    accounts: null,
+    sources: null,
+    wallets: null,
+    categories: null,
+    tags: null,
   }
 
   handleChangeFileInput = event => {
@@ -15,9 +19,24 @@ class App extends Component {
     const reader = new FileReader();
     console.log('file', file.value);
     reader.onload = ev => {
-        console.log(ev.target.result);
-        this.setState({ text: ev.target.result })
-        console.log('parsed', Papa.parse(ev.target.result, { delimiter: '\n\n' }));
+        console.log('init string', ev.target.result);
+        const endFirstString = ev.target.result.indexOf('\n');
+        const newString = ev.target.result.substring(endFirstString + 1);
+        const newString1 = newString.split('\n\n\n');
+        console.log(newString1);
+        const arr = newString1.map(function(el, i) {
+          console.log(`parsed${i}`, Papa.parse(el, { header: true }));
+          return Papa.parse(el, { header: true })
+        });
+        console.log(this);
+        this.setState({
+          accounts: arr[0],
+          sources: arr[1],
+          wallets: arr[2],
+          categories: arr[3],
+          tags: arr[4],
+        });
+        console.log('newString', newString);
     };
     reader.readAsText(file);
   }
